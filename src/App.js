841d912fe38b44492f0c3e690633b550ffa986a2
Turtled <trtld2@gmail.com>
2019-09-16 21:41:49 -0700

    mvp complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.css';
+import { Route } from 'react-router-dom';
 import { Home, About, Contact, Navigation } from './components';
 
 const App = () => (
   <div>
+    <Route exact path="/" component={Home} />
+    <Route exact path="/about" component={About} />
+    <Route exact path="/contact" component={Contact} />
     <Navigation />
   </div>
 );
